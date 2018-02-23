174272e8d07b52c739aad35f6700d039c0053957
IvanBrasilico <brasilico.ivan@gmail.com>
2018-02-23 11:10:03 -0300

    debug multi ports

--- wsgi_debug.py
+++ wsgi_debug.py
@@ -1,8 +1,11 @@
 import os
-
 os.environ['DEBUG'] = '1'
 
+from ajna_commons.flask.conf import VIRASANA_URL
 from virasana.app import app
 
 if __name__ == '__main__':
-    app.run()
+    port = 5000
+    if VIRASANA_URL:
+        port = int(VIRASANA_URL.split(':')[-1])
+    app.run(port=port)

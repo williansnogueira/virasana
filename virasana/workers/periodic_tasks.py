de40685a3582574ef000f060a5579d5db0d38d1e
IvanBrasilico <brasilico.ivan@gmail.com>
2019-02-22 17:56:11 -0300

    Alerta - mais possibilidades
    Levantar casos de contêineres com alerta e checar XML

--- virasana/workers/periodic_tasks.py
+++ virasana/workers/periodic_tasks.py
@@ -7,8 +7,16 @@ Aqui ficam as rotinas que serão chamadas periodicamente.
 
 """
 
+import time
+from datetime import datetime, timedelta
 from virasana.workers.tasks import celery, processa_bson, processa_carga,\
     processa_predictions
+from ajna_commons.flask.conf import (BACKEND, BROKER, BSON_REDIS, DATABASE,
+                                     MONGODB_URI, redisdb)
+from pymongo import MongoClient
+
+from virasana.integracao import xmli
+from virasana.scripts.predictionsupdate import predictions_update
 
 
 @celery.on_after_configure.connect
@@ -24,3 +32,21 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(61 * 60.0, processa_predictions.s())  # 61 min
     sender.add_periodic_task(6 * 3603.0, processa_carga.s())  # 6h
     # sender.add_periodic_task(12 * 3600.00, processa_stats.s())  # 12h
+
+
+if __name__=='__main__':
+    with MongoClient(host=MONGODB_URI) as conn:
+        db = conn[DATABASE]
+        s0 = time.time() + timedelta(days=1)
+        while True:
+            time.sleep(2)
+            if time.time() - s0 > (15*60):
+                print("Iniciando atualizações...")
+                doisdias = datetime.now() - timedelta(days=2)
+                num5 = xmli.dados_xml_grava_fsfiles(db, 2000, doisdias)
+                predictions_update('ssd', 'bbox', 500, 4)
+                predictions_update('index', 'index', 500, 4)
+                predictions_update('vaziosvm', 'vazio', 500, 4)
+                predictions_update('peso', 'peso', 500, 4)
+                s0 = time.time()
+

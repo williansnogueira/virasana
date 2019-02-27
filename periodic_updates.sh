9c5e9e9b4e51f4099a1a27185a157bfbe0dc1a6b
IvanBrasilico <brasilico.ivan@gmail.com>
2019-02-27 17:25:00 -0300

    --

--- periodic_updates.sh
+++ periodic_updates.sh
@@ -2,6 +2,6 @@
 # Adicionar no crontab:
 # sudo crontab -e
 # Ex: rodar a cada 2 horas
-# 0 */2 * * * su -u ivan "/home/ivan/ajna/virasana/periodic_update.sh"
+# 0 */2 * * * su -u ivan "/home/ivan/ajna/virasana/periodic_updates.sh"
 
 /home/ivan/ajna/virasana/virasana-venv/bin/python virasana/scripts/periodic_updates.py >> /var/log/virasana/periodic_updates.log

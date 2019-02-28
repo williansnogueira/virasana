60e47a999ceb1a95c5e629ab55d714eabf348e5e
brasilico.ivan@gmail.com <Iv@n1234>
2019-02-28 18:30:49 -0300

    exemplo cron periodic_updates

--- periodic_updates.sh
+++ periodic_updates.sh
@@ -3,5 +3,6 @@
 # sudo crontab -e
 # Ex: rodar a cada 2 horas
 # 0 */2 * * * su -u ivan "/home/ivan/ajna/virasana/periodic_updates.sh"
-
+# touch /var/log/virasana/periodic_updates.log
+# chown -U ivan /var/log/virasana/periodic_updates.log
 /home/ivan/ajna/virasana/virasana-venv/bin/python virasana/scripts/periodic_updates.py >> /var/log/virasana/periodic_updates.log

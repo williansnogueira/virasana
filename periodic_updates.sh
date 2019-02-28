0ba4dbe63ecd4cc8cbcbd226c11d1ba7e02b3b9c
brasilico.ivan@gmail.com <Iv@n1234>
2019-02-28 19:47:34 -0300

    exemplo cron periodic_updates e correcao 2>&1 para log

--- periodic_updates.sh
+++ periodic_updates.sh
@@ -5,4 +5,4 @@
 # 0 */2 * * * su -u ivan "/home/ivan/ajna/virasana/periodic_updates.sh"
 # touch /var/log/virasana/periodic_updates.log
 # chown -U ivan /var/log/virasana/periodic_updates.log
-/home/ivan/ajna/virasana/virasana-venv/bin/python virasana/scripts/periodic_updates.py >> /var/log/virasana/periodic_updates.log
+/home/ivan/ajna/virasana/virasana-venv/bin/python virasana/scripts/periodic_updates.py >> /var/log/virasana/periodic_updates.log 2&>1

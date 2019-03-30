a4e9c2899758a692db6c14bfeb46b2ce17e3f958
brasilico.ivan@gmail.com <Iv@n1234>
2019-03-29 23:57:49 -0300

    Testa mongo_dict

--- virasana/integracao/carga2/conhecimento.py
+++ virasana/integracao/carga2/conhecimento.py
@@ -87,7 +87,7 @@ def conhecimentos_containers_faltantes(dict_faltantes, dict_conhecimentos):
     return dict_conhecimentos_filtrado
 
 
-def monta_mongo_dict(db, dict_conhecimentos_containeres, dict_faltantes):
+def monta_mongo_dict(db, dict_conhecimentos_containeres):
     containers_set = set(dict_conhecimentos_containeres.keys())
     conhecimentos_set = set()
     for lista in dict_conhecimentos_containeres.values():
@@ -97,10 +97,18 @@ def monta_mongo_dict(db, dict_conhecimentos_containeres, dict_faltantes):
     containers = mongo_find_in(db, 'CARGA.Container', ['conhecimento', 'container'],
                                [conhecimentos_set, containers_set], 'container')
     conhecimentos = mongo_find_in(db, 'CARGA.Conhecimento', ['conhecimento'],
-                               [conhecimentos_set], 'conhecimento')
+                                  [conhecimentos_set], 'conhecimento')
     ncms = mongo_find_in(db, 'CARGA.NCM', ['conhecimento'],
-                               [conhecimentos_set], 'conhecimento')
+                         [conhecimentos_set], 'conhecimento')
+    manifestos_conhecimentos = mongo_find_in(db, 'CARGA.ManifestoConhecimento', ['conhecimento'],
+                                  [conhecimentos_set], 'conhecimento')
+    manifestos_set = set()
+    for conhecimento, linha in manifestos_conhecimentos.items():
+        print(linha)
+        manifestos_set.add(linha['manifesto'])
     manifestos = mongo_find_in(db, 'CARGA.Manifesto', ['manifesto'], [manifestos_set], 'manifesto')
+    print(manifestos_set)
+    print(manifestos)
     manifestos_escala = mongo_find_in(db, 'CARGA.EscalaManifesto', ['manifesto'], [manifestos_set], 'manifesto')
     escalas_set = set([value['escala'] for value in manifestos_escala.values()])
     escalas = mongo_find_in(db, 'CARGA.Escala', ['escala'], [escalas_set], 'escala')
@@ -109,11 +117,10 @@ def monta_mongo_dict(db, dict_conhecimentos_containeres, dict_faltantes):
     for container, values in containers.items():
         ldict = {'vazio': True}
         ldict['container'] = values
-        manifesto = values['manifesto']
-        escala = manifestos_escala[manifesto]['escala']
-        ldict['manifesto'] = manifestos[manifesto]
-        ldict['escala'] = escalas[escala]
-        ldict['atracacao'] = atracacoes[escala]
+        ldict['conhecimento'] = conhecimentos
+        ldict['manifesto'] = manifestos
+        ldict['escala'] = escalas
+        ldict['ncm'] = ncms
         mongo_dict[container] = ldict
 
     return mongo_dict
@@ -134,8 +141,8 @@ def conhecimento_grava_fsfiles(db, data_inicio, data_fim, importacao=True):
                                                get_cursor)
     dict_conhecimentos_containeres = conhecimentos_containers_faltantes(
         dict_faltantes, dict_conhecimentos)
-    total_conhecimentos = len(dict_conhecimentos_containeres.keys())
-    logger.info('Total de conhecimentos para estes contêineres de %s a %s: %s' %
+    total_conhecimentos = len(dict_conhecimentos_containeres)
+    logger.info('Total de contêineres com conhecimento de %s a %s: %s' %
                 (data_inicio, data_fim, total_conhecimentos))
 
     dados_carga = monta_mongo_dict(db,
@@ -156,3 +163,45 @@ def conhecimento_grava_fsfiles(db, data_inicio, data_fim, importacao=True):
         'Encontrados %s .'
         % (total_fsfiles, len(dados_carga))
     )
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from pymongo import MongoClient
+    from ajna_commons.flask.conf import DATABASE, MONGODB_URI
+
+    db = MongoClient(host=MONGODB_URI)[DATABASE]
+
+    # Testa mongo_dict
+    row = db.fs.files.find_one({'metadata.contentType': 'image/jpeg',
+                                'metadata.carga.vazio': False},
+                               {'metadata.carga': 1})
+
+    container = row['metadata']['carga']['container'][0]['container']
+    conhecimentos = [item['conhecimento']
+                     for item in row['metadata']['carga']['conhecimento']]
+    dict_conhecimentos_containeres = {
+        container: conhecimentos
+    }
+    dict_faltantes = {
+        container: row['_id']
+    }
+    dados_carga = monta_mongo_dict(db,
+                                   dict_conhecimentos_containeres)
+
+    dados_fsfiles = row['metadata']['carga']
+    dados_carga = dados_carga[container]
+    # print(dados_carga)
+    # print(dados_fsfiles)
+    for key, value in dados_carga.items():
+        value_fsfiles = dados_fsfiles.get(key)
+        if value_fsfiles is None:
+            print('%s não existe' % key)
+            continue
+        if type(value) != type(value_fsfiles):
+            print('Tipos diferentes: novo %s antigo %s. (%s) ' %
+                  (type(value), type(value_fsfiles), key))
+        if value_fsfiles != value:
+            print('Valores diferentes: %s' % key)
+            print(value)
+            print(value_fsfiles)
+    assert dados_carga == dados_fsfiles

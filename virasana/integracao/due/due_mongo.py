9e1050098e3b9680d1eb0bfec2758950bb5726bb
IvanBrasilico <brasilico.ivan@gmail.com>
2019-09-10 15:17:29 -0300

    indices Mongo

--- virasana/integracao/due/due_mongo.py
+++ virasana/integracao/due/due_mongo.py
@@ -1,5 +1,5 @@
-
 import json
+import pymongo
 from bson import ObjectId
 
 
@@ -13,3 +13,37 @@ def update_due(db, dues):
             {'$set': {'metadata.due': due}}
         )
         print(result)
+
+
+CHAVES_DUE = [
+    'metadata.due.numero',
+    'metadata.due.Declarante',
+    'metadata.due.itens.Exportador',
+    'metadata.due.itens.paisImportador',
+    'metadata.due.itens.recintoAduaneiroDespacho.codigo',
+    'metadata.due.itens.recintoAduaneiroEmbarque.codigo',
+]
+
+
+def create_indexes(db):
+    """Utilitário. Cria índices relacionados à integração.
+
+    São criados índices para desempenho nas consultas.
+    Alguns índices únicos também são criados, estes para evitar importação
+    duplicada do mesmo registro.
+    """
+    for campo in CHAVES_DUE:
+        try:
+            db['fs.files'].create_index(campo)
+        except pymongo.errors.OperationFailure:
+            pass
+
+
+
+if __name__ == '__main__':  # pragma: no cover
+    from pymongo import MongoClient
+    from ajna_commons.flask.conf import DATABASE, MONGODB_URI
+
+    db = MongoClient(host=MONGODB_URI)[DATABASE]
+    print('Criando índices para DUE')
+    create_indexes(db)

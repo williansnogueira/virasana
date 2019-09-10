7db430808f30a76db08e122708c86725b1f5923c
IvanBrasilico <brasilico.ivan@gmail.com>
2019-09-10 15:39:42 -0300

    Exibir campos DUE nos filtros personalizados e em Aba na tela view_file

--- virasana/integracao/due/due_mongo.py
+++ virasana/integracao/due/due_mongo.py
@@ -17,9 +17,10 @@ def update_due(db, dues):
 
 CHAVES_DUE = [
     'metadata.due.numero',
+    'metadata.due.ruc',
     'metadata.due.Declarante',
+    'metadata.due.PaisImportador',
     'metadata.due.itens.Exportador',
-    'metadata.due.itens.paisImportador',
     'metadata.due.itens.recintoAduaneiroDespacho.codigo',
     'metadata.due.itens.recintoAduaneiroEmbarque.codigo',
 ]

bda911c871cc432e2e3b242f0b0dc4e4a121588e
IvanBrasilico <brasilico.ivan@gmail.com>
2019-11-04 14:51:28 -0300

    Caminhos integracao mercante

--- virasana/scripts/periodic_updates.py
+++ virasana/scripts/periodic_updates.py
@@ -17,20 +17,24 @@ import sys
 import time
 from datetime import date, datetime, timedelta
 from json.decoder import JSONDecodeError
+from sqlalchemy import create_engine
 
 import requests
 from pymongo import MongoClient
 
 from ajna_commons.flask.conf import (DATABASE,
-                                     MONGODB_URI, VIRASANA_URL)
+                                     MONGODB_URI, SQL_URI,
+                                     VIRASANA_URL)
 from ajna_commons.flask.log import logger
 
 from virasana.integracao.carga2.carga2 import do_update_carga
 from virasana.integracao import atualiza_stats, \
     carga, get_service_password, info_ade02, xmli
+from virasana.integracao.mercante import processa_xml_mercante
 from virasana.scripts.gera_indexes import gera_indexes
 from virasana.scripts.predictionsupdate import predictions_update2
 from virasana.models import anomalia_lote
+
 
 def get_token(session, url):
     """Faz um get na url e tenta encontrar o csrf_token na resposta."""
@@ -81,10 +85,10 @@ def reload_indexes():
     return result
 
 
-def periodic_updates(db, lote=2000):
+def periodic_updates(db, connection, lote=2000):
     print('Iniciando atualizações...')
     hoje = datetime.combine(date.today(), datetime.min.time())
-    doisdias =  hoje - timedelta(days=2)
+    doisdias = hoje - timedelta(days=2)
     cincodias = hoje - timedelta(days=5)
     dezdias = hoje - timedelta(days=10)
     ontem = hoje - timedelta(days=1)
@@ -92,6 +96,8 @@ def periodic_updates(db, lote=2000):
     # TODO: Testar e se funcionar colocar a nova integração do CARGA
     #  (linha comentadao abaixo) e tirar a antiga (próxima linha)
     # do_update_carga(db)
+    processa_xml_mercante.get_arquivos_novos(connection)
+    processa_xml_mercante.xml_para_mercante(connection)
     carga.dados_carga_grava_fsfiles(db, lote * 10, dezdias)
     anomalia_lote.processa_zscores(db, cincodias, ontem)
     info_ade02.adquire_pesagens(db, cincodias, ontem)
@@ -109,18 +115,19 @@ def periodic_updates(db, lote=2000):
 if __name__ == '__main__':
     os.environ['DEBUG'] = '1'
     logger.setLevel(logging.DEBUG)
-    with MongoClient(host=MONGODB_URI) as conn:
-        db = conn[DATABASE]
-        daemonize = '--daemon' in sys.argv
-        periodic_updates(db)
-        s0 = time.time() - 600
-        counter = 1
-        while daemonize:
-            logger.info('Dormindo 10 minutos... ')
-            logger.info('Tempo decorrido %s segundos.' % (time.time() - s0))
-            time.sleep(30)
-            if time.time() - s0 > 300:
-                logger.info('Periódico chamado rodada %s' % counter)
-                counter += 1
-                periodic_updates(db)
-                s0 = time.time()
+    with create_engine(SQL_URI) as connection:
+        with MongoClient(host=MONGODB_URI) as conn:
+            db = conn[DATABASE]
+            daemonize = '--daemon' in sys.argv
+            periodic_updates(db, connection)
+            s0 = time.time() - 600
+            counter = 1
+            while daemonize:
+                logger.info('Dormindo 10 minutos... ')
+                logger.info('Tempo decorrido %s segundos.' % (time.time() - s0))
+                time.sleep(30)
+                if time.time() - s0 > 300:
+                    logger.info('Periódico chamado rodada %s' % counter)
+                    counter += 1
+                    periodic_updates(db, connection)
+                    s0 = time.time()

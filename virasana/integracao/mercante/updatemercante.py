b15ed57c090175189ffc78284cd98b500ba4813b
IvanBrasilico <brasilico.ivan@gmail.com>
2019-11-04 14:44:33 -0300

    Caminhos integracao mercante

--- virasana/integracao/mercante/updatemercante.py
+++ virasana/integracao/mercante/updatemercante.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from ajna_commons.flask.conf import SQL_URI
 from virasana.integracao.mercante.processa_xml_mercante import get_arquivos_novos, \
     xml_para_mercante
-from integracao.mercante.resume_mercante import mercante_resumo
+from virasana.integracao.mercante.resume_mercante import mercante_resumo
 
 
 def do():
